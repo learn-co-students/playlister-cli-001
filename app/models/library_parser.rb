bb8f97c130502554e2ea0f62bceb42a3e7fde732
Spencer Rogers <spencer1248@gmail.com>
2014-02-05 16:45:13 -0500

    Make some tests pass

---
+++ app/models/library_parser.rb
@@ -0,0 +1,28 @@
+class LibraryParser
+  def files
+    data_path = File.join(File.dirname(__FILE__), '..', '..', 'db', 'data')
+    @files ||= Dir.entries(data_path)[2..-1]
+  end
+
+  def self.parse
+    self.new.call
+  end
+
+  def parse_filename(filename)
+    artist_match = filename.match(/^(.*) -/)
+    song_match   = filename.match(/- (.*) \[/)
+    genre_match  = filename.match(/\[([^\]]*)\]/)
+
+    artist = artist_match && artist_match[1]
+    song   = song_match   && song_match[1]
+    genre  = genre_match  && genre_match[1]
+
+    [artist, song, genre]
+  end
+
+  def call
+    files.each do |filename|
+      raw_data = parse_filename(filename)
+    end
+  end
+end
