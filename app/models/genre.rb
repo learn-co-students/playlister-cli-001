3a84a1313f9553b36ff4eb890c67621c19d8dcac
Spencer Rogers <spencer1248@gmail.com>
2014-02-05 17:12:59 -0500

    Update genre model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,30 @@
 class Genre
-  attr_accessor :name
+  attr_reader :name, :url
+
+  @@genres = []
+
+  def initialize
+    @@genres << self
+  end
+
+  def name=(name)
+    @name = name
+    @url = "#{parameterize(name)}.html"
+  end
+
+  def parameterize(string)
+    string.downcase.gsub(' ', '-')
+  end
+
+  def self.all
+    @@genres
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
 end
