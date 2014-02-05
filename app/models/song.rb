41e99ffbeac0a2e0fb0099afb41d881710705256
Spencer Rogers <spencer1248@gmail.com>
2014-02-05 16:59:55 -0500

    Implement song methods with no dependencies

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,26 @@
 class Song
-  attr_accessor :name
+  attr_reader :name, :url
+
+  @@songs = []
+
+  def self.reset_all
+    @@songs.clear
+  end
+
+  def name=(name)
+    @name = name
+    @url = "#{parameterize(name)}.html"
+  end
+
+  def parameterize(string)
+    string.downcase.gsub(' ', '-')
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.all
+    @@songs
+  end
 end
