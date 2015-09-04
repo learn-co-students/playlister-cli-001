17f51e2022269ad2d8b858c60c7a5abe6092820d
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2015-09-04 14:50:09 -0400

    Update library_parser.rb

--- app/models/library_parser.rb
+++ app/models/library_parser.rb
@@ -9,30 +9,9 @@ class LibraryParser
   end
 
   def parse_filename(filename)
-  
-    # at this point a file name looks something like this: 
-    # "db/data/Action Bronson - Larry Csonka - indie.mp3"
-
-    # the line below slices off the db/data/ that prepends each file name
-  
     filename.slice!(0..7)
-
-    # now, we need to grab the artist name, song name and genre name out of the filename string
-
-    # right now, the filename looks something like this: "Action Bronson - Larry Csonka - indie.mp3"
-
-    # first, get rid of the '.mp3' file extension:
-
     filename.slice!(-4..-1)
-
-    # now, split the string on the ' - '
-
     info = filename.split(" - ")
-    
-    # our resultant `info` array looks like this: ["Action Bronson", "Larry Csonka", "indie"]
-    # we are therefore successfully returning the array of artist, song, genre
-
- 
   end
 
   def build_song(filename)
