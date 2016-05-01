09e592a841895d29eef698906843feaec3bc4c35
epang1 <epang1@gmail.com>
2016-05-01 16:07:38 -0400

    done

--- app/models/library_parser.rb
+++ app/models/library_parser.rb
@@ -1,11 +1,15 @@
 class LibraryParser
 
  def call
-  # code here
+  files
+  @files.each do |song|
+    build_song(song)
+  end
  end
 
   def files
     @files = Dir['db/data/*']
+
   end
 
   def parse_filename(filename)
@@ -15,7 +19,25 @@ class LibraryParser
   end
 
   def build_song(filename)
-    # code here!
+    song_parts = parse_filename(filename)
+    new_song = Song.new
+    if Artist.find_by_name(song_parts[0]) == nil
+      new_artist = Artist.new
+      new_artist.name = song_parts[0]
+      new_song.artist = new_artist
+    else
+      new_song.artist = Artist.find_by_name(song_parts[0])
+    end
+    new_song.name = song_parts[1]
+
+    if Genre.find_by_name(song_parts[2]) == nil
+      new_genre = Genre.new
+      new_genre.name = song_parts[2]
+      new_song.genre = new_genre
+    else
+      new_song.genre = Genre.find_by_name(song_parts[2])
+    end
+    new_song
   end
 
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,48 @@
-class Song
-  # code here
 
-  def self.reset_all
+class Song
+
+extend Findable
+extend Create
+extend Reset
+
+attr_accessor :name
+attr_reader :genre, :artist
+
+def self.all=(something)
+  @@all = something
+end
+
+def initialize
+  if @@all == nil
+    @@all = []
+  else
+    @@all << self
   end
+end
+
+def self.all
+  @@all
+end
+
+def self.count
+  @@all.count
+end
+
+def genre=(genre)
+  @genre = genre
+  genre.add_song(self)
+end
+
+def artist=(artist)
+  @artist = artist
+  if @artist.songs.include?(self) == false
+    @artist.add_song(self)
+  end
+end
+
+def to_s
+  return "#{@artist.name} - #{self.name} [#{@genre.name}]"
+end
+
+
 end
